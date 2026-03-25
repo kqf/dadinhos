3273a5b76ca80d3725ed484e9b3873ee15a5cc46
kqf <q.dev.null@gmail.com>
2026-03-25 07:03:25 +0000

    Fix the objects

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,7 +1,12 @@
 
-from detasks.objects import make_objects
+from detasks.objects import distribution_count, distribution_size, make_objects
 
 
 def test_objects():
-    objects = make_objects()
+    objects = make_objects(
+        n_samples=100,
+        distribution_count=distribution_count,
+        distribution_size=distribution_size,
+
+    )
     print(objects)
